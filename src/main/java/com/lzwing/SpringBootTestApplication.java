6d54a64aa2a25b015f1e592a4ada1dc296ba8352
longzhiwuing <czy3332842@gmail.com>
2018-05-11 09:52:50 +0800

    add retry kafka

--- src/main/java/com/lzwing/SpringBootTestApplication.java
+++ src/main/java/com/lzwing/SpringBootTestApplication.java
@@ -3,10 +3,12 @@ package com.lzwing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 //@EnableMQConfiguration
 @EnableConfigurationProperties
+@EnableRetry
 public class SpringBootTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lzwing/mq/kafka/KafkaMessageProducer.java
+++ src/main/java/com/lzwing/mq/kafka/KafkaMessageProducer.java
@@ -1,18 +1,14 @@
 package com.lzwing.mq.kafka;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
-@Component
+//@Component
 @EnableScheduling
 @Slf4j
 public class KafkaMessageProducer {
