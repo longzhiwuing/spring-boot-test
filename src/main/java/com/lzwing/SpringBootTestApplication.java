62eb2475833315d91557995865c03236f893ecff
longzhiwuing <czy3332842@gmail.com>
2018-08-14 14:34:46 +0800

    添加自定义配置读取例子

--- src/main/java/com/lzwing/SpringBootTestApplication.java
+++ src/main/java/com/lzwing/SpringBootTestApplication.java
@@ -3,13 +3,11 @@ package com.lzwing;
 import com.lzwing.importselector.annotation.EnableLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 //@EnableMQConfiguration
-@EnableConfigurationProperties
 @EnableRetry
 @ServletComponentScan
 //测试自定义enable
